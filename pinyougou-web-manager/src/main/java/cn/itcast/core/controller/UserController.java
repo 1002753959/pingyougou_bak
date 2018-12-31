7fa2027e00c8290825b279be0df6f9f78974c4b1
123 <123@itcast.com>
2018-12-31 10:38:02 +0800

    whx 03

---
+++ pinyougou-web-manager/src/main/java/cn/itcast/core/controller/UserController.java
@@ -0,0 +1,75 @@
+package cn.itcast.core.controller;
+//                            _ooOoo_
+//                           o8888888o
+//                           88" . "88
+//                           (| -_- |)
+//                            O\ = /O
+//                        ____/`---'\____
+//                      .   ' \\| |// `.
+//                       / \\||| : |||// \
+//                     / _||||| -:- |||||- \
+//                       | | \\\ - /// | |
+//                     | \_| ''\---/'' | |
+//                      \ .-\__ `-` ___/-. /
+//                   ___`. .' /--.--\ `. . __
+//                ."" '< `.___\_<|>_/___.' >'"".
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+//                 \ \ `-. \_ __\ /__ _/ .-` / /
+//         ======`-.____`-.___\_____/___.-`____.-'======
+//                            `=---='
+//
+//         .............................................
+//                  佛祖镇楼                  BUG辟易
+//          佛曰:
+//                  写字楼里写字间，写字间里程序员；
+//                  程序人员写程序，又拿程序换酒钱。
+//                  酒醒只在网上坐，酒醉还来网下眠；
+//                  酒醉酒醒日复日，网上网下年复年。
+//                  但愿老死电脑间，不愿鞠躬老板前；
+//                  奔驰宝马贵者趣，公交自行程序员。
+//                  别人笑我忒疯癫，我笑自己命太贱；
+//                  不见满街漂亮妹，哪个归得程序员？
+//         .............................................
+
+import cn.itcast.core.entity.PageResult;
+import cn.itcast.core.entity.Result;
+import cn.itcast.core.pojo.good.Brand;
+import cn.itcast.core.service.UserService;
+import cn.itcast.core.service.UserService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: Chenqi
+ * @Description:
+ * @Date: Create in 0:56 2018/11/28
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Reference
+    private UserService userService;
+
+
+    /**
+     * 分页显示
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("/queryPage")
+    public PageResult queryPage(int pageNum , int pageSize){
+        PageResult pageResult = userService.queryPage(pageNum, pageSize);
+        return pageResult;
+    }
+
+
+
+}
